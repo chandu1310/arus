ef7eff24932783ed28f75bc276f0be46f76471a4
chandrashekar.ch <chandrashekar.ch@gmail.com>
2010-11-09 12:44:46 +0000

    Created new files: Resource and Indexer. Wrote initial code. Pending: Index logic.

--- src/arus/indexer/Indexer.java
+++ src/arus/indexer/Indexer.java
@@ -1,5 +1,47 @@
 package arus.indexer;
 
-public class Indexer {
+import java.io.File;
 
+import arus.indexer.model.Resource;
+
+public class Indexer {
+	private String INST_TOP;
+
+	private Resource rsc;
+
+	private boolean isIndexed = false;
+
+	public Indexer(String INST_TOP) throws Exception
+	{
+		this.INST_TOP = INST_TOP;
+		reIndex();
+	}
+
+	public boolean indexed()
+	{
+		return this.isIndexed;
+	}
+
+	public Resource getIndexedResource()
+	{
+		if (isIndexed)
+			return this.rsc;
+		else
+			return null;
+	}
+
+	public void reIndex() throws Exception
+	{
+		if(rsc == null || !isIndexed )    
+		{
+			// TODO : By uday
+			//  Indexing logic to be written
+			//Clue: Understand java.io.File class
+
+			File f = new File("c:/dummy.txt");
+			if( ! f.exists() )
+			    f.createNewFile();
+		}
+		isIndexed = true;
+    }
 }
--- src/arus/indexer/model/Resource.java
+++ src/arus/indexer/model/Resource.java
@@ -1,5 +1,50 @@
 package arus.indexer.model;
 
-public class Resource {
+import java.util.ArrayList;
 
+public class Resource {
+	private String filename;
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	private String version;
+	private boolean isDir = false;
+	private ArrayList<Resource> resoucesList;
+
+	public Resource(boolean isDir) {
+		this.isDir = isDir;
+		if (isDir)
+			resoucesList = new ArrayList<Resource>();
+	}
+
+	public void addResource(Resource rsc) {
+		if (this.isDir) {
+			resoucesList.add(rsc);
+		}
+	}
+
+	public Resource[] getResourceList() {
+		return (Resource[]) this.resoucesList.toArray();
+	}
+
+	// print all the file names and directory names of this resource.
+	// if the resource has folder type resources, call toString on them.
+	public String toString() {
+		String value = "";
+		return value;
+	}
 }
